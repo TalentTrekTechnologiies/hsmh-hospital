32a7b8b7dd33f3cb020fed931d6a705345ccd649
Janaki2302 <janaki.t@talenttrektechnologies.com>
2026-02-13 09:37:08 +0530

    created admin files

---
+++ src/pages/admin/AppointmentsAdmin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AppointmentsAdmin() {
+  return (
+    <div>AppointmentsAdmin</div>
+  )
+}
+
+export default AppointmentsAdmin
---
+++ src/pages/admin/DoctorsAttendance.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DoctorsAttendance() {
+  return (
+    <div>DoctorsAttendance</div>
+  )
+}
+
+export default DoctorsAttendance
